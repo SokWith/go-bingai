75159eed204081fe851356b2142027b22b2c6c59
SokWith <129967025+SokWith@users.noreply.github.com>
2024-09-24 14:24:25 +0800

    Update sydney.go

--- common/api/sydney.go
+++ common/api/sydney.go
@@ -7,9 +7,22 @@ import (
 )
 
 func Sydney(w http.ResponseWriter, r *http.Request) {
+	// 检查并删除 USRLOC cookie
+	if _, err := r.Cookie("USRLOC"); err == nil {
+		c := &http.Cookie{
+			Name:   "USRLOC",
+			MaxAge: -1,
+		}
+		http.SetCookie(w, c)
+	}
+
+	// 检查用户是否已认证
 	if !helper.CheckAuth(r) {
 		helper.UnauthorizedResult(w)
 		return
 	}
+
+	// 代理请求到 BING_SYDNEY_URL
 	common.NewSingleHostReverseProxy(common.BING_SYDNEY_URL).ServeHTTP(w, r)
 }
+
